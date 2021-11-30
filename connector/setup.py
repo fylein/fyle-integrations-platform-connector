c5cf28d244b81af7863e5a737b9d003b17c37b32
ashwinthanaraj <ashwin.t@fyle.in>
2021-11-30 10:31:59 +0530

    Fyler->Spender changes

--- connector/setup.py
+++ connector/setup.py
@@ -5,7 +5,7 @@ with open('../README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='fyle-integrations-platform-connector',
-    version='0.3.2',
+    version='1.0.0',
     author='Shwetabh Kumar',
     author_email='shwetabh.kumar@fyle.in',
     description='A common platform connector for all the Fyle Integrations to interact with Fyle Platform APIs',
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'fyle_accounting_mappings>=1.6.0',
-        'fyle>=v0.16.0'
+        'fyle>=v0.19.0'
     ],
     classifiers=[
         'Topic :: Internet :: WWW/HTTP',
