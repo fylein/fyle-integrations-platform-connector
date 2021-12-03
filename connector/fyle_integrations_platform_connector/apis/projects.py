8598f0467fd434ae2a2e7337330a950e38721c05
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-12-03 14:32:59 +0530

    Fix projects sync for sub project and Expense Custom Field sync (#9)

--- connector/fyle_integrations_platform_connector/apis/projects.py
+++ connector/fyle_integrations_platform_connector/apis/projects.py
@@ -6,3 +6,22 @@ class Projects(Base):
 
     def __init__(self):
         Base.__init__(self, attribute_type='PROJECT', query_params={'is_enabled': 'eq.true'})
+
+
+    def sync(self):
+        """
+        Syncs the latest API data to DB.
+        """
+        generator = self.get_all_generator()
+        for items in generator:
+            project_attributes = []
+            for project in items['data']:
+                if not project['sub_project']:
+                    project_attributes.append({
+                        'attribute_type': self.attribute_type,
+                        'display_name': self.attribute_type.replace('_', ' ').title(),
+                        'value': project['name'],
+                        'source_id': project['id']
+                    })
+
+            self.bulk_create_or_update_expense_attributes(project_attributes)
