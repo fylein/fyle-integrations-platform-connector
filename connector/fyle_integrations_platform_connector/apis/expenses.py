c4ac423192a03e9233a2b2caf9955fe9a6781609
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-04-29 11:31:45 +0530

    support last_paid_at filter in fetching expenses (#27)
    support last_paid_at filter in fetching expenses (#27)

--- connector/fyle_integrations_platform_connector/apis/expenses.py
+++ connector/fyle_integrations_platform_connector/apis/expenses.py
@@ -10,7 +10,7 @@ class Expenses(Base):
     """Class for Expenses APIs."""
 
     def get(self, source_account_type: List[str], state: str, last_synced_at: datetime=None,
-        settled_at: datetime=None, filter_credit_expenses: bool=False) -> List[dict]:
+        settled_at: datetime=None, filter_credit_expenses: bool=False, last_paid_at=None) -> List[dict]:
         """
         Get expenses.
 
@@ -25,7 +25,7 @@ class Expenses(Base):
         """
         all_expenses = []
 
-        query_params = self.__construct_expenses_query_params(source_account_type, state, last_synced_at, settled_at)
+        query_params = self.__construct_expenses_query_params(source_account_type, state, last_synced_at, settled_at, last_paid_at)
         generator = self.connection.list_all(query_params)
 
         for expense_list in generator:
@@ -38,7 +38,7 @@ class Expenses(Base):
 
 
     @staticmethod
-    def __construct_expenses_query_params(source_account_type: List[str], state: str, updated_at: datetime, settled_at: datetime) -> dict:
+    def __construct_expenses_query_params(source_account_type: List[str], state: str, updated_at: datetime, settled_at: datetime, last_paid_at: datetime) -> dict:
         """
         Construct expenses query params.
         :param source_account_type: Source account types.
@@ -74,6 +74,10 @@ class Expenses(Base):
         if settled_at:
             settled_at = 'gte.{}'.format(datetime.strftime(settled_at, '%Y-%m-%dT%H:%M:%S.000Z'))
             query_params['last_settled_at'] = settled_at
+
+        if last_paid_at:
+            last_paid_at = 'gte.{}'.format(datetime.strftime(last_paid_at, '%Y-%m-%dT%H:%M:%S.000Z'))
+            query_params['report->last_paid_at'] = last_paid_at
 
         return query_params
 
