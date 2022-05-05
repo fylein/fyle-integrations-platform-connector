47374d398248cc2b7e692c8a9f96d3e25a76d838
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-05-05 16:40:37 +0530

    fix

--- connector/fyle_integrations_platform_connector/apis/categories.py
+++ connector/fyle_integrations_platform_connector/apis/categories.py
@@ -7,7 +7,6 @@ class Categories(Base):
     def __init__(self):
         Base.__init__(self, attribute_type='CATEGORY', query_params={'is_enabled': 'eq.true'})
 
-
     def sync(self):
         """
         Syncs the latest API data to DB.
@@ -16,10 +15,9 @@ class Categories(Base):
         for items in generator:
             category_attributes = []
             for category in items['data']:
-                if category['name'] != category['sub_category'] and category['sub_category']:
+                if category['sub_category'] and category['name'] != category['sub_category']:
                     category['name'] = '{0} / {1}'.format(category['name'], category['sub_category'])
-                else:
-                    category['name'] = '{0}'.format(category['name'])
+
                 category_attributes.append({
                     'attribute_type': self.attribute_type,
                     'display_name': self.attribute_type.replace('_', ' ').title(),
