08ae3578ac771ec1f3cd6cc048c9e3419e7ec365
Nilesh Pant <nileshpant6969@gmail.com>
2023-02-21 19:49:15 +0530

    minor add

--- connector/fyle_integrations_platform_connector/apis/expense_fields.py
+++ connector/fyle_integrations_platform_connector/apis/expense_fields.py
@@ -8,3 +8,13 @@ class ExpenseField(Base):
 
     def __init__(self):
         Base.__init__(self, attribute_type='EXPENSE_FIELDS')
+
+
+    def sync(self):
+        """
+        Syncs the latest API data to DB.
+        """
+        generator = self.get_all_generator()
+        for items in generator:
+            for expense_field in items['data']:
+                self.create_or_update_expense_fields(expense_field)
