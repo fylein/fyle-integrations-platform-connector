8e8d4a6c63e854c30cb16b6693b4ad4047860b5a
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-02-13 20:10:30 +0530

    Bumped up the fyle sdk version

--- connector/setup.py
+++ connector/setup.py
@@ -5,7 +5,7 @@ with open('../README.md', 'r', encoding='utf-8') as f:
 
 setuptools.setup(
     name='fyle-integrations-platform-connector',
-    version='1.36.2',
+    version='1.36.3',
     author='Shwetabh Kumar',
     author_email='shwetabh.kumar@fyle.in',
     description='A common platform connector for all the Fyle Integrations to interact with Fyle Platform APIs',
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'fyle_accounting_mappings>=1.25.0',
-        'fyle>=v0.35.0'
+        'fyle>=v0.36.1'
     ],
     classifiers=[
         'Topic :: Internet :: WWW/HTTP',
