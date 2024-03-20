cd09bcad97e5c789a1ca4e49584bd168930dfc67
ruuushhh <rushikeshtayade2001@gmail.com>
2024-03-20 17:35:10 +0530

    handle Delete case for categories and project

--- connector/fyle_integrations_platform_connector/apis/categories.py
+++ connector/fyle_integrations_platform_connector/apis/categories.py
@@ -1,5 +1,10 @@
 from .base import Base
+import logging
 from datetime import datetime
+from fyle_accounting_mappings.models import ExpenseAttributesDeletionCache
+
+logger = logging.getLogger(__name__)
+logger.level = logging.INFO
 
 class Categories(Base):
     """Class for Categories APIs."""
@@ -11,21 +16,35 @@ class Categories(Base):
         """
         Syncs the latest API data to DB.
         """
-        generator = self.get_all_generator(sync_after=sync_after)
-        for items in generator:
-            category_attributes = []
-            for category in items['data']:
-                if self.attribute_is_valid(category):
-                    if category['sub_category'] and category['name'] != category['sub_category']:
-                        category['name'] = '{0} / {1}'.format(category['name'], category['sub_category'])
+        try:
+            expense_attributes_deletion_cache, _ = ExpenseAttributesDeletionCache.objects.get_or_create(workspace_id=self.workspace_id)
+            generator = self.get_all_generator()
+            
+            for items in generator:
+                category_attributes = []
+                for category in items['data']:
+                    expense_attributes_deletion_cache = ExpenseAttributesDeletionCache.objects.get(workspace_id=self.workspace_id)
+                    expense_attributes_deletion_cache.category_ids.append(category['id'])
+                    expense_attributes_deletion_cache.save()
+                    if self.attribute_is_valid(category):
+                        if category['sub_category'] and category['name'] != category['sub_category']:
+                            category['name'] = '{0} / {1}'.format(category['name'], category['sub_category'])
 
-                    category_attributes.append({
-                        'attribute_type': self.attribute_type,
-                        'display_name': self.attribute_type.replace('_', ' ').title(),
-                        'value': category['name'],
-                        'source_id': category['id'],
-                        'active': category['is_enabled'],
-                        'detail': None
-                    })
+                        category_attributes.append({
+                            'attribute_type': self.attribute_type,
+                            'display_name': self.attribute_type.replace('_', ' ').title(),
+                            'value': category['name'],
+                            'source_id': category['id'],
+                            'active': category['is_enabled'],
+                            'detail': None
+                        })
 
-            self.bulk_create_or_update_expense_attributes(category_attributes, True)
+                self.bulk_create_or_update_expense_attributes(category_attributes, True)
+
+            self.bulk_update_deleted_expense_attributes()
+
+        except Exception as exception:
+            logger.exception(exception)
+            expense_attributes_deletion_cache = ExpenseAttributesDeletionCache.objects.get(workspace_id=self.workspace_id)
+            expense_attributes_deletion_cache.category_ids = []
+            expense_attributes_deletion_cache.save()
--- connector/fyle_integrations_platform_connector/apis/projects.py
+++ connector/fyle_integrations_platform_connector/apis/projects.py
@@ -1,6 +1,10 @@
 from .base import Base
 from datetime import datetime
+import logging
+from fyle_accounting_mappings.models import ExpenseAttributesDeletionCache
 
+logger = logging.getLogger(__name__)
+logger.level = logging.INFO
 
 class Projects(Base):
     """Class for Projects APIs."""
@@ -13,20 +17,35 @@ class Projects(Base):
         """
         Syncs the latest API data to DB.
         """
-        generator = self.get_all_generator(sync_after)
-        for items in generator:
-            project_attributes = []
-            for project in items['data']:
-                if self.attribute_is_valid(project):
-                    if project['sub_project']:
-                        project['name'] = '{0} / {1}'.format(project['name'], project['sub_project'])
+        try:
+            expense_attributes_deletion_cache, _ = ExpenseAttributesDeletionCache.objects.get_or_create(workspace_id=self.workspace_id)
+            generator = self.get_all_generator()
 
-                    project_attributes.append({
-                        'attribute_type': self.attribute_type,
-                        'display_name': self.attribute_type.replace('_', ' ').title(),
-                        'value': project['name'],
-                        'active': project['is_enabled'],
-                        'source_id': project['id']
-                    })
+            for items in generator:
+                project_attributes = []
+                for project in items['data']:
+                    expense_attributes_deletion_cache = ExpenseAttributesDeletionCache.objects.get(workspace_id=self.workspace_id)
+                    expense_attributes_deletion_cache.project_ids.append(project['id'])
+                    expense_attributes_deletion_cache.save()
+                    if self.attribute_is_valid(project):
+                        if project['sub_project']:
+                            project['name'] = '{0} / {1}'.format(project['name'], project['sub_project'])
 
-            self.bulk_create_or_update_expense_attributes(project_attributes, True)
+                        project_attributes.append({
+                            'attribute_type': self.attribute_type,
+                            'display_name': self.attribute_type.replace('_', ' ').title(),
+                            'value': project['name'],
+                            'active': project['is_enabled'],
+                            'source_id': project['id']
+                        })
+
+                self.bulk_create_or_update_expense_attributes(project_attributes, True)
+            
+            self.bulk_update_deleted_expense_attributes()
+
+        except Exception as exception:
+            logger.exception(exception)
+            expense_attributes_deletion_cache = ExpenseAttributesDeletionCache.objects.get(workspace_id=self.workspace_id)
+            expense_attributes_deletion_cache.project_ids = []
+            expense_attributes_deletion_cache.save()
+            
