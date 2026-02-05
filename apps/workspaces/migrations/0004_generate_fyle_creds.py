00507846121336be643da2ed2ab4c694bce389bb
Ashwin Thanaraj <ashwin.t@fyle.in>
2026-02-05 11:50:03 +0530

    chore: replace old migration with dummy domain (#129)

--- apps/workspaces/migrations/0004_generate_fyle_creds.py
+++ apps/workspaces/migrations/0004_generate_fyle_creds.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
         ('workspaces', '0003_fylecredential'),
     ]
 
-    query = "insert into fyle_credentials (refresh_token, cluster_domain, workspace_id, created_at, updated_at) values ('{}', 'https://staging.fyle.tech', 1, date('now'), date('now'))".format(os.environ.get('REFRESH_TOKEN'))
+    query = "insert into fyle_credentials (refresh_token, cluster_domain, workspace_id, created_at, updated_at) values ('{}', 'https://www.dummydomain.com', 1, date('now'), date('now'))".format(os.environ.get('REFRESH_TOKEN'))
 
     operations = [
         migrations.RunSQL(query)
